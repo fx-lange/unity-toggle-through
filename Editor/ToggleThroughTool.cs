66d3d4eaecc913a590560783b5ec53c6e27feadf
Felix Lange <langefx@gmail.com>
2021-09-19 00:13:05 +0200

    initial commit

---
+++ Editor/ToggleThroughTool.cs
@@ -0,0 +1,115 @@
+using UnityEditor;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+namespace FxLange.ToggleThrough
+{
+    [EditorTool("Toggle Through")]
+    internal class ToggleThroughTool : EditorTool
+    {
+        [SerializeField] private Texture2D toolIcon;
+
+        private GUIContent _iconContent;
+
+        private int _activeIndex;
+
+        private void OnEnable()
+        {
+            _iconContent = new GUIContent()
+            {
+                image = toolIcon,
+                text = "Toggle Through Tool",
+                tooltip = "Toggle through a selection of GameObjects"
+            };
+        }
+
+        public override GUIContent toolbarIcon => _iconContent;
+
+        public override void OnActivated()
+        {
+            Selection.selectionChanged += OnSelectionChanged;
+
+            Init();
+        }
+
+        public override void OnWillBeDeactivated()
+        {
+            Selection.selectionChanged -= OnSelectionChanged;
+
+            Clear();
+        }
+
+        private void OnSelectionChanged()
+        {
+            Clear();
+            Init();
+        }
+
+        private void Clear()
+        {
+        }
+
+        private void Init()
+        {
+            if (Selection.gameObjects is {Length: >= 2})
+            {
+                _activeIndex = Selection.gameObjects[0].activeSelf ? 0 : -1;
+            }
+            else
+            {
+                _activeIndex = -1;
+            }
+        }
+
+        private void Toggle()
+        {
+            var selection = Selection.gameObjects;
+
+            if (selection.Length == 1)
+            {
+                var singleSelection = selection[0];
+                singleSelection.SetActive(!singleSelection.activeSelf);
+                return;
+            }
+
+            _activeIndex++;
+            _activeIndex %= selection.Length;
+
+            for (var index = 0; index < selection.Length; index++)
+            {
+                var gameObject = selection[index];
+                gameObject.SetActive(index == _activeIndex);
+            }
+        }
+
+        public override void OnToolGUI(EditorWindow window)
+        {
+            if (window is not SceneView)
+            {
+                return;
+            }
+
+            EditorGUI.BeginChangeCheck();
+            
+            Handles.BeginGUI();
+            using (new GUILayout.HorizontalScope())
+            {
+                using (new GUILayout.VerticalScope(EditorStyles.helpBox))
+                {
+                    if (GUILayout.Button("Toggle Through"))
+                    {
+                        Toggle();
+                    }
+                }
+
+                GUILayout.FlexibleSpace();
+            }
+            Handles.EndGUI();
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObjects(Selection.objects, "Toggle GameObjects");
+            }
+        }
+    }
+}
